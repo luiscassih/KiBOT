f663c3aa1190bd8e416d1d198f48b3234e188b4a
Luis Cassih <luiscassih@protonmail.com>
2018-01-20 21:56:50 -0300

    Basic electron and webapp separation

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var express = require("express");
 var app = express();
-
+var url = require("url");
+var path = require("path");
+const electron = require('electron');
+const pug = require("pug");
+const locals = {
+    version: "0.2.0"
+};
+const electronPug = require("electron-pug")({pretty: true, locals});
+const BrowserWindow = electron.BrowserWindow;
 //var robot = require("robotjs");
 
 
 
 // App config
-app.set("port", process.argv[2] || 3000);
+var webServerPort = 3000;
+process.argv.forEach((argv, i ) => {
+    if (argv == "--port" && !isNaN(process.argv[i+1])) {
+        webServerPort =  process.argv[i+1];
+    }
+});
+app.set("port", webServerPort);
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static("public"));
@@ -21,3 +35,17 @@ controllers.loadControllers(app);
 app.listen(app.get("port"), function(){
     console.log("Server listen on port " + app.get("port"));
 });
+
+
+
+// Starting Electron
+let win;
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "electron/index.pug"),
+        protocol: "file:",
+        slashes: true
+    }));
+}
+electron.app.on("ready", createWindow);
